312107c2032b1c5c799aa837e0a97a165aaf0e13
juancorreaxsmarts <juan.correa@xmarts.com>
2022-02-22 15:41:19 -0400

    nuevo modelo

--- l10n_do_update_sequence/models/models.py
+++ l10n_do_update_sequence/models/models.py
@@ -9,3 +9,16 @@ class IrSequence(models.Model):
 
     l10n_latam_journal_id = fields.Many2one('account.journal', 'Journal')
     l10n_latam_document_type_id = fields.Many2one('l10n_latam.document.type', 'Document Type')
+
+class AccoMoveLine(models.Model):
+
+    _inherit = 'account.move.line'
+
+    always_set_currency_id = fields.Many2one('res.currency', string='Foreign Currency',
+        compute='_compute_always_set_currency_id',
+        help="Technical field used to compute the monetary field. As currency_id is not a required field, we need to use either the foreign currency, either the company one.")
+
+    @api.depends('currency_id')
+    def _compute_always_set_currency_id(self):
+        for line in self:
+            line.always_set_currency_id = line.currency_id or line.company_currency_id
