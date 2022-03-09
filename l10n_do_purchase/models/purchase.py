a6282c04d8b4cd43dc0c9eb2468bc98be6160f55
juancorreaxsmarts <juan.correa@xmarts.com>
2022-03-09 00:23:29 -0400

    cambios compras

--- l10n_do_purchase/models/purchase.py
+++ l10n_do_purchase/models/purchase.py
@@ -8,21 +8,21 @@ class PurchaseOrder(models.Model):
 
         res = super(PurchaseOrder, self).action_view_invoice()
 
-        ctx = res["context"]
+    #    ctx = res["context"]
 
-        is_l10n_do = (
-            self.env["res.company"].browse(ctx["default_company_id"]).country_id.code
-            == "DO"
-            and self.env["account.move"]
-            .with_context(
-                default_type=ctx["default_type"],
-                default_company_id=ctx["default_company_id"],
-            )
-            ._get_default_journal()
-            .l10n_latam_use_documents
-        )
+    #    is_l10n_do = (
+    #        self.env["res.company"].browse(ctx["default_company_id"]).country_id.code
+    #        == "DO"
+    #        and self.env["account.move"]
+    #        .with_context(
+    #            default_type=ctx["default_type"],
+    #            default_company_id=ctx["default_company_id"],
+    #        )
+    #        ._get_default_journal()
+    #        .l10n_latam_use_documents
+    #    )
 
-        if is_l10n_do:
-            del ctx["default_ref"]
+    #    if is_l10n_do:
+    #        del ctx["default_ref"]
 
         return res
